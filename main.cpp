a562a306050186d328af8597914882e127940a00
Danielken11 <105623990+Danielken11@users.noreply.github.com>
2023-11-14 22:01:42 +0200

    Second commit ,caesar cipher improved with second key word

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <string>
 #include <algorithm>
+#include <unordered_set>
 
 //global variables...
 
@@ -21,7 +22,7 @@ void clrscr(){system("cls");}
 void print_start(){
 
     std::cout << "\n        Cifrul Cezar\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
-    std::cout << "1)Criptare\n2)Decriptare\n3)Brute Force\n4)Iesire\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
+    std::cout << "1)Criptare 1 cheie\n2)Decriptare 1 cheie\n3)Criptare 2 chei\n4)Decriptare 2 chei\n5)Brute Force\n6)Iesire\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
     
 }
 
@@ -202,6 +203,177 @@ std::string cesar_encryption_brutte_force(std::string input,int k){
 
             return final_input;
 }   
+//Functiile pentru criptare si decriptare avansata a cifrului caesar cu 2 chei , una dintre care va face parte din alfabet fiind un cuvant secret...
+std::string encryption_cesarAdvanced(){
+
+    std::cin.ignore();
+    std::cout << "Introduceti textul dorit criptarii: "; std::getline(std::cin,text);
+
+    std::transform(text.begin(),text.end(),text.begin(), ::toupper);
+
+    std::string final_text = removeSpaces(text);
+
+    std::cout << "Textul Introdus: " << final_text;
+
+    int k;
+    int current_number;
+    int changer;
+    std::string keyWord;
+
+    std::cout << "\nIntroduceti cheia pentru criptare de la 1 pana la 25: "; std::cin >> k;
+    std::cin.ignore();
+    std::cout << "\r\nIntroduceti cuvantul cheie format din literele alfabetului latin cu o lungime nu mai mica de 7";std::getline(std::cin,keyWord);
+    std::transform(keyWord.begin(),keyWord.end(),keyWord.begin(), ::toupper);
+
+    std::string advancedAlphabeth = keyWord + alphabeth;
+    std::string alpha;
+    std::unordered_set<char> char_counter;
+
+    for(char character : advancedAlphabeth){
+        if (char_counter.find(character) == char_counter.end())
+        {
+            alpha += character;
+            char_counter.insert(character);
+        }
+    }
+
+    if (k >= 1 && k <= 25 && keyWord.length() > 7){
+
+    for ( i = 0; i < final_text.length(); i++){
+        for ( j = 0; j < alpha.length(); j++ ){
+
+            if(final_text[i] == alpha[j]){
+                break;
+            }
+        }
+
+         if (j != alpha.length()) {
+            int current_number = j;
+            int changer = (current_number + k) % alpha.length();
+
+            final_text[i] = alpha[changer];
+            was_crypted = true;
+        
+        }
+    }
+      std::cout << "\r\nTextul criptat:\n " << final_text;
+        text_for_decryption = final_text;
+
+    }else{
+
+        std::cout << "\r\nCheia trebuie sa fie de la 1 pana la 25 si cuvantul cheie mai mare de 7 caractere";
+    }
+
+    
+    return final_text;
+}
+
+std::string decryption_cesarAdvanced(){
+
+//this variable in case if no user have a new encrypted text for us...
+        int k;
+        int current_number;
+        int changer;
+        std::string keyWord;
+        
+
+    if(was_crypted == true){
+        
+        std::cout << "\r\nDoriti sa decriptati textul criptat anterior: " << text_for_decryption << "\r\napasati 1 daca nu apasati 2 ";
+        int chooser;
+        std::cin >> chooser;
+
+        if(chooser == 1){
+            std::cout << "\r\nIntroduceti cheia necesa decriptarii: "; std::cin >> k;
+             std::cin.ignore();
+             std::cout << "\r\nIntroduceti cuvantul cheie\n";std::getline(std::cin,keyWord);
+                std::transform(keyWord.begin(),keyWord.end(),keyWord.begin(), ::toupper);
+
+                std::string advancedAlphabeth = keyWord + alphabeth;
+                std::string alpha;
+                std::unordered_set<char> char_counter;
+
+            for(char character : advancedAlphabeth){
+                 if (char_counter.find(character) == char_counter.end()){
+                        alpha += character;
+                        char_counter.insert(character);
+                    }
+                }
+
+            if (k >= 1 && k <= 25 && keyWord.length() > 7 ){
+            for(i = 0; i < text_for_decryption.length(); i++){
+
+                for (j = 0; j < alpha.length(); j++){
+                     if(text_for_decryption[i] == alpha[j]){
+                         break;
+                    }   
+                }
+
+                 if (j != alpha.length()) {
+                     int current_number = j;
+                        int changer = (current_number - k + alpha.length()) % alpha.length();
+
+                    text_for_decryption[i] = alpha[changer];
+                }
+            }
+
+        }else{
+            std::cout << "\r\nCheia trebuie sa fie de la 1 pana la 25 si cuvantul cheie mai mare de 7 caractere";
+        }
+            std::cout << "\r\nTextul decriptat este: " << text_for_decryption << "\n";
+            print_important_info();
+
+            }else if (chooser == 2){
+                was_crypted = false;
+            }
+        }else if (was_crypted == false){
+
+               std::cin.ignore();
+               std::string textChanger;
+        std::cout << "\r\nIntroduceti textul dorit decriptarii: "; std::getline(std::cin,textChanger);
+        text_for_decryption = removeSpaces(textChanger);
+         std::transform(text_for_decryption.begin(),text_for_decryption.end(),text_for_decryption.begin(), ::toupper);
+        std::cout << "\r\nTextul pentru decriptare: " << text_for_decryption; 
+
+        std::cout << "\r\nIntroduceti cheia de la 1 pana la 25: "; std::cin >> k;
+         std::cin.ignore();
+             std::cout << "\r\nIntroduceti cuvantul cheie\n";std::getline(std::cin,keyWord);
+                std::transform(keyWord.begin(),keyWord.end(),keyWord.begin(), ::toupper);
+
+                std::string advancedAlphabeth = keyWord + alphabeth;
+                std::string alpha;
+                std::unordered_set<char> char_counter;
+
+            for(char character : advancedAlphabeth){
+                 if (char_counter.find(character) == char_counter.end()){
+                        alpha += character;
+                        char_counter.insert(character);
+                    }
+                }
+
+        if (k >= 1 && k <= 25 && keyWord.length() > 7){
+            for(i = 0; i < text_for_decryption.length(); i++){
+
+                for (j = 0; j < alpha.length(); j++){
+                     if(text_for_decryption[i] == alpha[j]){
+                         break;
+                    }   
+                }
+                 if (j != alpha.length()) {
+                     int current_number = j;
+                        int changer = (current_number - k + alpha.length()) % alpha.length();
+
+                    text_for_decryption[i] = alpha[changer];
+                }
+            }
+        }else{
+            std::cout << "\r\nCheia trebuie sa fie de la 1 pana la 25 si cuvantul cheie mai mare de 7 caractere";
+        }
+            std::cout << "\r\nTextul decriptat este: " << text_for_decryption << "\n";
+    }
+    return text_for_decryption;
+}
+
 
 int main(void){
 
@@ -215,14 +387,17 @@ int main(void){
         switch(userInput){
         case 1:clrscr();encryption_cesar();break;
         case 2:clrscr();decryption_cesar(); break;
-        case 3:clrscr();std::cout << "\r\nIntrodu textul pentru brute force: ";
-         std::cin.ignore(); std::getline(std::cin,text_brute);
+        case 3:clrscr();encryption_cesarAdvanced();break;
+        case 4:clrscr();decryption_cesarAdvanced();break;
+        case 5:clrscr();
+            std::cout << "\r\nIntrodu textul pentru brute force: ";
+                std::cin.ignore(); std::getline(std::cin,text_brute);
         
             for(int i = 0; i<26; i++){
                 cesar_encryption_brutte_force(text_brute,i);
             }
             break;
-        case 4:exit(0); break;
+        case 6:exit(0); break;
             default:std::cout << "\nError!!!" ;break;
     }
 }
